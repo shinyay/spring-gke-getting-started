287c68d6cf9f8c06b59348456faed58ddfb13d5d
shinyay <shinya.com@gmail.com>
2020-01-20 20:00:20 +0900

    Add a script for deleting a container image on GCR

---
+++ script/delete-image.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+
+function delete_image
+  argparse -n delete-image 'h/help' 'i/image=' -- $argv
+  or return 1
+
+  if set -lq _flag_help
+    echo "delete-image.fish -i/--image <CONTAINER_IMAGE_NAME>"
+    return
+  end
+
+  if set -lq _flag_image
+    set -lq _flag_image
+  else
+    echo "delete-image.fish -i/--image <CONTAINER_IMAGE_NAME>" & return
+  end
+
+  gcloud container images delete --force-delete-tags --quiet $_flag_image
+  gcloud container images list
+end
+
+delete_image $argv
